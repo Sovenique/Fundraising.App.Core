b909ec4b8612466f5c0826e5490b0ceaa2ee8d9f
Nikos Barollis <nbarollis@athtech.gr>
2021-05-19 15:29:24 +0300

    Create Project Servise Interface

---
+++ Fundraising.App.Core/Interfaces/IProjectService.cs
@@ -0,0 +1,18 @@
+﻿using Fundraising.App.Core.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundraising.App.Core.Interfaces
+{
+    public interface IProjectService
+    {
+        public OptionsProject CreateProject(OptionsProject optionProjrct);
+        public List<OptionsProject> GetAllProjects();
+        public OptionsProject GetOptionsProjectById(int id);
+        public OptionsProject UpdateProject(OptionsProject optionsProject, int id);
+        public bool DeleteProject(int id);
+    }
+}
