f9f57fba2589e6e2c7b8443fdd646b676ad90ee1
vasilas.msi <vasilas.cei@gmail.com>
2021-05-19 14:46:05 +0300

    Commit Project Entity

---
+++ Fundraising.App.Core/Entities/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundraising.App.Core.Entities
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public ProjectStatus ProjectStatus { get; set; }
+        public Member Creator { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public decimal AmountGathered { get; set; }
+        public decimal TargetAmount { get; set; }
+        public List<Reward> Rewards { get; set; }
+    }
+}
