dc956a79d01ff177c5dc3d7b31bd10b84b617107
Nikos Barollis <nbarollis@athtech.gr>
2021-05-19 15:09:25 +0300

    migration

--- Fundraising.App.Core/Database/Connection.cs
+++ Fundraising.App.Core/Database/Connection.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Fundraising.App.Entities;
+﻿using Fundraising.App.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Fundraising.App.Database
 {
