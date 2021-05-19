bbda477342ad512e82309c69c378f25937399111
Christos Leventis-Rigas <cleventis@athtech.gr>
2021-05-19 14:42:36 +0300

    Options: Member, Payment Database

---
+++ Fundraising.App.Core/Database/Connection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Fundraising.App.Entities;
+
+namespace Fundraising.App.Database
+{
+    internal class FundraisingAppDbContext : DbContext
+    {
+        public DbSet<Member> Member { get; set; }
+        public DbSet<Project> Project { get; set; }
+        public DbSet<Reward> Reward { get; set; }
+        public DbSet<Payment> Payment { get; set; }
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = localhost; Initial Catalog= Fundraising.App; Integrated Security = true");
+        }
+    }
+}
