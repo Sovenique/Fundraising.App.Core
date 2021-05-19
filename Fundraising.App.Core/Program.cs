e35fc6b401d5b81811c4cede64bf3b7b8cc805d3
Christos Leventis-Rigas <cleventis@athtech.gr>
2021-05-19 14:34:02 +0300

    Add project files.

---
+++ Fundraising.App.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fundraising.App.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
