190ef9c223a774866c39b22f6fe3f8d0609b8a2f
vasilas.msi <vasilas.cei@gmail.com>
2021-05-19 14:50:35 +0300

    commit Reward entity and ProjectStatus Enumeration

---
+++ Fundraising.App.Core/Entities/ProjectStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundraising.App.Core.Entities
+{
+    public enum ProjectStatus
+    {
+        ON_HOLD,        // It will not accept new payments until reopened
+        IN_PROGRESS,    // It will accept new payments
+        COMPLETED       // It will not accept any other payments
+    }
+}
---
+++ Fundraising.App.Core/Entities/Reward.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundraising.App.Core.Entities
+{
+    public class Reward
+    {
+        public int Id { set; get; }
+        public string Description { set; get; }
+        public int ProjectId { get; set; }
+        public List<Payment> Payments { set; get; }
+    }
+}
