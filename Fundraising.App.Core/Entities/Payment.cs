69ac31f154459e55ff4ab646c35cb9692b353e66
Ioli Giannakopoulou <igiannakopoulou@athtech.gr>
2021-05-19 14:49:21 +0300

    Memeber and Payment

---
+++ Fundraising.App.Core/Entities/Payment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fundraising.App.Entities
+{
+    public class Payment
+    {
+        public decimal Amount { get; set;}
+        public int Id { get; set; }
+        public Member Backer { get; set; }
+        public Reward Reward { get; set; }
+        public DateTime PaymentDate { get; set; }
+            
+      
+    }
+}
