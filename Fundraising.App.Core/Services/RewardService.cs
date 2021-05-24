bd9446a9c1a7eb866ca03b6fdca42464fee6f95f
Christos Leventis-Rigas <cleventis@athtech.gr>
2021-05-25 02:49:12 +0300

    Reward Fixes

--- Fundraising.App.Core/Services/RewardService.cs
+++ Fundraising.App.Core/Services/RewardService.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Fundraising.App.Core.Services
