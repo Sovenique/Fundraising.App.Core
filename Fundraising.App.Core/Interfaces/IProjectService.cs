ada08658fdda2050b98b57631199538bbc5aa5c2
Nikos Barollis <nbarollis@athtech.gr>
2021-05-26 12:14:25 +0300

    fix Project Service

--- Fundraising.App.Core/Interfaces/IProjectService.cs
+++ Fundraising.App.Core/Interfaces/IProjectService.cs
@@ -1,15 +1,18 @@
-﻿using Fundraising.App.Core.Options;
+﻿using Fundraising.App.Core.Entities;
+using Fundraising.App.Core.Models;
+using Fundraising.App.Core.Options;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
 
 namespace Fundraising.App.Core.Interfaces
 {
     public interface IProjectService
     {
-        public OptionsProject CreateProject(OptionsProject optionProjrct);
-        public List<OptionsProject> GetAllProjects();
-        public OptionsProject GetOptionsProjectById(int Id);
-        public OptionsProject UpdateProject(OptionsProject optionsProject, int Id);
-        public bool DeleteProject(int Id);
+
+        Task<Result<Project>> CreateProjectAsync(OptionsProject optionProject);
+        Task<Result<int>> DeleteProjectByIdAsync(int Id);
+        Task<Result<List<Project>>> GetAllProjectsAsync();
+        Task<Result<Project>> GetProjectByIdAsync(int Id);
+        Task<Result<Project>> UpdateProjectByIdAsync(OptionsProject optionProject, int Id);
     }
 }
