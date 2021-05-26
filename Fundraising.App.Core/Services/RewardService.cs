f3e4c0d14ba9b65614e5f4c62ddd8655f726e7e6
Christos Leventis-Rigas <cleventis@athtech.gr>
2021-05-26 04:45:34 +0300

    Member CreatedDate input removed

--- Fundraising.App.Core/Services/RewardService.cs
+++ Fundraising.App.Core/Services/RewardService.cs
@@ -2,7 +2,6 @@
 using Fundraising.App.Core.Interfaces;
 using Fundraising.App.Core.Models;
 using Fundraising.App.Core.Options;
-using Fundraising.App.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
@@ -21,14 +20,14 @@ namespace Fundraising.App.Core.Services
             _dbContext = dbContext;
             _logger = logger;
         }
-
+        //CREATE
         public async Task<Result<Reward>> CreateRewardAsync(OptionReward optionReward)
         {
             if (optionReward == null)
             {
                 return new Result<Reward>(ErrorCode.BadRequest, "Null options.");
             }
-
+            //validation
             if (string.IsNullOrWhiteSpace(optionReward.Title) ||
               string.IsNullOrWhiteSpace(optionReward.Description))
             {
@@ -46,7 +45,7 @@ namespace Fundraising.App.Core.Services
             {
                 return new Result<Reward>(ErrorCode.Conflict, $"Reward with #{optionReward.Title} already exists.");
             }
-
+            //operation
             var newReward = new Reward
             {
                 Title = optionReward.Title,
@@ -75,16 +74,16 @@ namespace Fundraising.App.Core.Services
 
 
         }
-
+        //DELETE
         public async Task<Result<int>> DeleteRewardByIdAsync(int Id)
         {
             var rewardToDelete = await GetRewardByIdAsync(Id);
-
+            //validation
             if (rewardToDelete.Error != null || rewardToDelete.Data == null)
             {
                 return new Result<int>(ErrorCode.NotFound, $"Reward with id #{Id} not found.");
             }
-
+            //operation
             _dbContext.Rewards.Remove(rewardToDelete.Data);
 
             try
@@ -103,41 +102,43 @@ namespace Fundraising.App.Core.Services
                 Data = Id
             };
         }
-
+        //GET ALL
         public async Task<Result<List<Reward>>> GetAllRewardsAsync()
         {
             var rewards = await _dbContext.Rewards.ToListAsync();
-
+            //operation
             return new Result<List<Reward>>
             {
                 Data = rewards.Count > 0 ? rewards : new List<Reward>()
             };
         }
-
+        //GET SINGLE
         public async Task<Result<Reward>> GetRewardByIdAsync(int Id)
         {
+            //validation
             if (Id <= 0)
             {
                 return new Result<Reward>(ErrorCode.BadRequest, "Id cannot be less than or equal to zero.");
             }
-
+            //operation
             var reward = await _dbContext
                 .Rewards
                 .SingleOrDefaultAsync(cus => cus.Id == Id);
-
+            //validation
             if (reward == null)
             {
                 return new Result<Reward>(ErrorCode.NotFound, $"Reward with id #{Id} not found.");
             }
-
+            
             return new Result<Reward>
             {
                 Data = reward
             };
         }
-
+        //UPDATE
         public async Task<Result<Reward>>UpdateRewardByIdAsync(OptionReward optionReward, int Id)
         {
+            //validation
             if (optionReward == null)
             {
                 return new Result<Reward>(ErrorCode.BadRequest, "Null options.");
@@ -162,7 +163,7 @@ namespace Fundraising.App.Core.Services
             {
                 return new Result<Reward>(ErrorCode.NotFound, $"Reward with id #{Id} not found.");
             }
-
+            //operation
             reward.Title = optionReward.Title;
             reward.Description = optionReward.Description;
 
