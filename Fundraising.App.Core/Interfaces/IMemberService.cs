22bdf42ed35502f631ccd300d30c33dee31b4673
vasilas.msi <vasilas.cei@gmail.com>
2021-06-04 22:19:16 +0300

    Member Service only GetAllMembers()

---
+++ Fundraising.App.Core/Interfaces/IMemberService.cs
@@ -0,0 +1,14 @@
+﻿using Fundraising.App.Core.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundraising.App.Core.Interfaces
+{
+    public interface IMemberService
+    {
+        public List<OptionMember> GetAllMembers();
+    }
+}
