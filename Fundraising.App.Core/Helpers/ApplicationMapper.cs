a28b3e084c93d3cb12152201020a24e2ad331353
Christos Leventis-Rigas <cleventis@athtech.gr>
2021-06-07 05:20:33 +0300

    Added AutoMapper

---
+++ Fundraising.App.Core/Helpers/ApplicationMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Fundraising.App.Core.Entities;
+using Fundraising.App.Core.Options;
+
+namespace Fundraising.App.Core.Helpers
+{
+    class ApplicationMapper : Profile
+    {
+        public ApplicationMapper()
+        {
+            CreateMap<Project, OptionsProject>().ReverseMap();
+            CreateMap<Reward, OptionReward>().ReverseMap();
+            CreateMap<Payment, OptionPayment>().ReverseMap();
+        }
+    }
+}
