86428a8b036f04d95595f270deeb6eb04aa8c120
Nikos Barollis <nbarollis@athtech.gr>
2021-06-09 18:58:38 +0300

    fix home page

--- Fundraising.App.Web/Controllers/HomeController.cs
+++ Fundraising.App.Web/Controllers/HomeController.cs
@@ -1,21 +1,41 @@
-﻿using Fundraising.App.Web.Models;
+﻿using Fundraising.App.Core.Interfaces;
+using Fundraising.App.Core.Options;
+using Fundraising.App.Web.Models;
+using Fundraising.App.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Fundraising.App.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IProjectService _projectService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICurrentUserService currentUserService,
+            IProjectService projectService)
         {
             _logger = logger;
+            _currentUserService = currentUserService;
+            _projectService = projectService;
+
         }
 
         public IActionResult Index()
         {
+            var trendingProjects = _projectService.GetTrendingProjects(3);
+            List<OptionsProject> optionsProjects = new();
+            for (int i = 0; i < trendingProjects.Count; i++)
+            {
+                optionsProjects.Add(trendingProjects.ElementAt(i).Key);
+            }
+            ViewData["User"] = _currentUserService;
+            ViewData["TrendingProjects"] = optionsProjects;
             return View();
         }
 
@@ -33,6 +53,12 @@ namespace Fundraising.App.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public async Task<IActionResult> Home()
+        {
+            
+            
+            return View();
         }
     }
 }
